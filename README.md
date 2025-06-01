a477071fa8bcdbd92669f1b12816aebe3b204ff0
Joey Li <joey@joeyli.dev>
2025-06-01 14:20:52 +0200

    separate logic and inline

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ransac-line
 
-This is a repository containing the code for fast line fitting, meant to be used by my [VLP](https://github.com/einstein8612/VLP) project. As such it's highly optimised for this specific case and will not change, unless it improves performance for that case. The API might be a bit weird as a consequence.
+This is a repository containing the code for fast line fitting, meant to be used by my [VLP](https://github.com/einstein8612/VLP) project. As such it's highly optimised for this specific case and will not change, unless it improves performance for that case. The API might be a bit weird as a consequence. An important feature here, is that it always only returns the slope, as the fit I am aiming for personally is `y = ax`. This can be easily changed and you are welcome to do so yourself.
 
 # Tests
 ```bash
--- src/ransac_line.c
+++ src/ransac_line.c
@@ -9,6 +9,40 @@ static inline bool is_inlier_fast(float a, float x, float actual_y, float thresh
     return fabs(a*x - actual_y) < threshold;
 }
 
+static inline int get_random_index(int num_points) {
+    return rand() % num_points;
+}
+
+static inline int calculate_inliers(float a, float* xs, float* ys, int num_points, float threshold) {
+    int inliers_count = 0;
+    for (int i = 0; i < num_points; i++) {
+        if (is_inlier_fast(a, xs[i], ys[i], threshold)) {
+            inliers_count++;
+        }
+    }
+    return inliers_count;
+}
+
+static inline float ransac_line_basic(float* xs, float* ys, int num_points) {
+    // Randomly select two points
+    int idx1 = get_random_index(num_points);
+    int idx2 = get_random_index(num_points);
+
+    // Ensure idx1 and idx2 are different
+    while (idx2 == idx1) {
+        idx2 = get_random_index(num_points);
+    }
+
+    // Do the line fitting
+    float x1 = xs[idx1];
+    float y1 = ys[idx1];
+    float x2 = xs[idx2];
+    float y2 = ys[idx2];
+    float slope = (y2 - y1) / (x2 - x1);
+
+    return slope;
+}
+
 float ransac_line(float* xs, float* ys, int num_points, float threshold, int max_iterations) {
     // Seed the random number generator with the current time
     srand((unsigned int) time(NULL));
@@ -16,30 +50,10 @@ float ransac_line(float* xs, float* ys, int num_points, float threshold, int max
     int best_inliers = 0;
     float best_slope = 0.0f;
     for (int i = 0; i < max_iterations; i++) {
-        // Randomly select two points
-        int idx1 = rand() % num_points;
-        int idx2 = rand() % num_points;
-
-        // Ensure idx1 and idx2 are different
-        while (idx2 == idx1) {
-            idx2 = rand() % num_points;
-        }
-
-        // Do the line fitting
-        float x1 = xs[idx1];
-        float y1 = ys[idx1];
-        float x2 = xs[idx2];
-        float y2 = ys[idx2];
-        float slope = (y2 - y1) / (x2 - x1);
-        float intercept = y1 - slope * x1;
+        float slope = ransac_line_basic(xs, ys, num_points);
 
         // Count inliers
-        int inliers_count = 0;
-        for (int j = 0; j < num_points; j++) {
-            if (is_inlier_fast(slope, xs[j], ys[j], threshold)) {
-                inliers_count++;
-            }
-        }
+        int inliers_count = calculate_inliers(slope, xs, ys, num_points, threshold);
 
         // Update the best model if this one has more inliers
         if (inliers_count > best_inliers) {
